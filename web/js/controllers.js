81d98aaecf4243d556bf67b39fe0014b2366c1a5
aluminiumgeek <mihail265@gmail.com>
2014-07-06 18:40:43 +0400

    panel page

--- web/js/controllers.js
+++ web/js/controllers.js
@@ -29,20 +29,85 @@ app.controller('LoginCtrl', function($scope, $http, $rootScope, $location) {
 });
 
 app.controller('LogoutCtrl', function($scope, $rootScope, $http, $location) {
-
     if ($rootScope.token) {
         $http({
             url: '/api/user/auth',
             method: 'delete'
         }).success(function() {
-            $rootScope.set_token = false;
+            $rootScope.set_token(false);
             
             $location.path('/');
         })
     }
 });
 
-app.controller('PanelCtrl', function($scope) {
+app.controller('PanelCtrl', function($scope, $http) {
+    $scope.model = {
+        items: []
+    };
+    
+    $scope.append = function() {
+        if ($scope.model.newitem) {
+            $scope.model.items.push($scope.model.newitem);
+            $scope.model.newitem = '';
+        }
+    }
+    
+    $scope.send = function() {
+        if ($scope.model.items.length > 0) {
+            $scope.task_sending = true;
+        
+            var data = {
+                'items': $scope.model.items,
+                'priority': 2
+            };
+        
+            $http({
+                url: '/api/task',
+                method: 'post',
+                data: data
+            }).success(function(data) {
+                if (data.status == 'ok') {
+                    $scope.new_task_id = data.task_id;
+                }
+                
+                $scope.clear();
+                $scope.task_sending = false;
+            });
+        }
+    }
+    
+    $scope.clear = function() {
+        $scope.model.items = [];
+        $scope.model.newitem = '';
+    }
+    
+    $scope.get_tasks = function() {
+        $http({
+            url: '/api/tasks',
+            method: 'get',
+        }).success(function(data) {
+            data.tasks.map(function(item) {
+                switch (item.status) {
+                    case 1:
+                        item.status_name = 'Waiting';
+                        break;
+                    case 2:
+                        item.status_name = 'Success';
+                        break;
+                    case 3:
+                        item.status_name = 'Error';
+                        break;
+                }
+            });
+            
+            $scope.tasks = data.tasks;
+        });
+    }
+    
+    $scope.get_tasks();
+    
+    setInterval($scope.get_tasks, 1500);
 });
 
 app.controller('UsersCtrl', function($scope) {
